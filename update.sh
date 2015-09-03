791e805e2d84c4df82dcc380f3ce571db4b0798a
andreafspeziale <andreafspeziale@MacBook-Air-di-Andrea.local>
2015-09-03 12:03:37 +0200

    update update script

--- update.sh
+++ update.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 mkdocs build
-echo "static site builded."
+echo "STATIC FILE BUILDED"
 git checkout gh-pages
-echo "moved to gh-pages branch"
 git rm -r .
 git rm .
-echo "removing from repo production files"
+echo "removing from repo production files REMOVING FROM REPO PRODUCTION FILE"
 mv /Users/andreafspeziale/Repository/utils/site/css /Users/andreafspeziale/Repository/utils
 mv /Users/andreafspeziale/Repository/utils/site/fonts /Users/andreafspeziale/Repository/utils
 mv /Users/andreafspeziale/Repository/utils/site/git /Users/andreafspeziale/Repository/utils
@@ -15,5 +14,11 @@ mv /Users/andreafspeziale/Repository/utils/site/license /Users/andreafspeziale/R
 mv /Users/andreafspeziale/Repository/utils/site/mongodb /Users/andreafspeziale/Repository/utils
 mv /Users/andreafspeziale/Repository/utils/site/index.html /Users/andreafspeziale/Repository/utils
 rm -r site
+echo "PRODUCTION FILE UPDATED"
 git add .
 git status
+git commit -am "production updated"
+git push origin gh-pages
+git checkout master
+git status
+echo "PRODUCTION UPDATED NIGGAH!"
