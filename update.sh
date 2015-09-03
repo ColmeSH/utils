a2b2828571056910d20223ee7fc18497a219d4bd
andreafspeziale <andreafspeziale@MacBook-Air-di-Andrea.local>
2015-09-03 14:39:09 +0200

    update script fixed

--- update.sh
+++ update.sh
@@ -3,15 +3,12 @@ mkdocs build
 echo "STATIC FILE BUILDED"
 git checkout gh-pages
 git rm -r .
-git rm .
 echo "removing from repo production files REMOVING FROM REPO PRODUCTION FILE"
-mv /Users/andreafspeziale/Repository/utils/site/* /Users/andreafspeziale/Repository/utils
+mv site/* .
 rm -r site
 echo "PRODUCTION FILE UPDATED"
 git add .
-git status
 git commit -am "production updated"
 git push origin gh-pages
 git checkout master
-git status
 echo "PRODUCTION UPDATED NIGGAH!"
