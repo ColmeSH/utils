403e889c14259cb81267e33e6e476df1d743086f
andreafspeziale <andreafspeziale@MacBook-Air-di-Andrea.local>
2015-09-03 11:41:47 +0200

    update update script

--- update.sh
+++ update.sh
@@ -6,3 +6,12 @@ echo "moved to gh-pages branch"
 git rm -r .
 git rm .
 echo "removing from repo production files"
+mkdir delete
+mv /Users/andreafspeziale/Repository/utils/css /Users/andreafspeziale/Repository/utils/delete
+mv /Users/andreafspeziale/Repository/utils/fonts /Users/andreafspeziale/Repository/utils/delete
+mv /Users/andreafspeziale/Repository/utils/git /Users/andreafspeziale/Repository/utils/delete
+mv /Users/andreafspeziale/Repository/utils/img /Users/andreafspeziale/Repository/utils/delete
+mv /Users/andreafspeziale/Repository/utils/js /Users/andreafspeziale/Repository/utils/delete
+mv /Users/andreafspeziale/Repository/utils/license /Users/andreafspeziale/Repository/utils/delete
+mv /Users/andreafspeziale/Repository/utils/mongodb /Users/andreafspeziale/Repository/utils/delete
+mv /Users/andreafspeziale/Repository/utils/index.html /Users/andreafspeziale/Repository/utils/delete
